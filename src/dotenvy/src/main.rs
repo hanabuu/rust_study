cbdbddf8aed7ab599a34fda89760aaa3111c1a5d
hanabusa-t2 <hanabusa-t2@alsok.co.jp>
2026-01-09 10:06:08 +0900

    dotenvyの使い方追加

--- src/dotenvy/src/main.rs
+++ src/dotenvy/src/main.rs
@@ -2,8 +2,16 @@ use dotenvy::dotenv;
 use std::env;
 
 fn main() {
-    // load environment variables from .env file
+    // .envファイルから環境変数を読み込む
     dotenv().expect(".env file not found");
+
+    // 個別にとるパターン(env::var())
+    let app_version = env::var("APP_VERSION").expect("APP_VERSION not set");
+    println!("App Version: {}", app_version);
+    let app_prod = env::var("APP_PROD").unwrap_or("development".to_string());
+    println!("App Prod: {}", app_prod);
+
+    // 一括でとるパターン(env::vars())
     for (key, value) in env::vars() {
         if key.starts_with("APP_") {
             println!("{key}: {value}");
