650250a3b224e40fa92f2c511daa5e18e3f50c81
hanabusa-t2 <hanabusa-t2@alsok.co.jp>
2026-05-11 19:34:04 +0900

    同期非同期の追加。envファイルからのパラメータ読込追加

--- src/fetchUrl/src/main.rs
+++ src/fetchUrl/src/main.rs
@@ -1,15 +1,62 @@
 use std::error::Error;
+use tokio::runtime::Runtime;
 
-const TARGET_URL: &str = "http://10.116.164.94:3001/api/getDbDirFile";
+use dotenvy::dotenv;
+use std::env;
+
+/// 同期的にURLをフェッチする関数
+/// 
+/// # Errors
+/// - ネットワークエラーやJSONのパースエラーが発生する可能性があります。
+/// # Examples
+/// ```
+/// sync_fetch_url(&fetch_url).unwrap();
+/// ```
+fn sync_fetch_url(target: &str) -> Result<(), Box<dyn Error>> {
+  let response = reqwest::blocking::get(target)?;
+  let status = response.status();
+  let json: serde_json::Value = response.json()?;
+
+  println!("URL: {}", target);
+  println!("HTTP Status: {}", status);
+  println!("Response JSON: {}", json);
+
+  Ok(())
+}
+
+/// 非同期的にURLをフェッチする関数
+/// 
+/// # Errors
+/// - ネットワークエラーやJSONのパースエラーが発生する可能性があります。
+/// # Examples
+/// ```
+/// let runtime = Runtime::new().unwrap();
+/// runtime.block_on(async_fetch_url()).unwrap();
+/// ```
+async fn async_fetch_url(target: &str) -> Result<(), Box<dyn Error>> {
+  let client = reqwest::Client::new();  
+  let response = client.get(target).send().await?;
+  let status = response.status();
+  let json: serde_json::Value = response.json().await?;
+
+  println!("URL: {}", target);
+  println!("HTTP Status: {}", status);
+  println!("Response JSON: {}", json);
+
+  Ok(())
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let response = reqwest::blocking::get(TARGET_URL)?;
-	let status = response.status();
-	let json: serde_json::Value = response.json()?;
 
-	println!("URL: {}", TARGET_URL);
-	println!("HTTP Status: {}", status);
-	println!("Response JSON: {}", json);
+  dotenv().expect(".env file not found");
+  let fetch_url = env::var("FetchURL").expect("FetchURL not set");
 
-	Ok(())
+  println!("=== Sync request ===");
+  sync_fetch_url(&fetch_url)?;
+
+  println!("=== Async request ===");
+  let runtime = Runtime::new()?;
+  runtime.block_on(async_fetch_url(&fetch_url))?;
+
+  Ok(())
 }
